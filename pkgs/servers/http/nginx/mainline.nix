fd99cd54c2c8faead35a8fbe85f51065a77c8c3b
ajs124 <git@ajs124.de>
2021-12-11 10:18:30 +0000

    nginx: remove openssl_3_0 override

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -1,6 +1,6 @@
-{ callPackage, openssl_3_0, ... }@args:
+{ callPackage, ... }@args:
 
-callPackage ./generic.nix (args // { openssl = openssl_3_0; }) {
+callPackage ./generic.nix args {
   version = "1.21.4";
   sha256 = "1ziv3xargxhxycd5hp6r3r5mww54nvvydiywcpsamg3i9r3jzxyi";
 }
